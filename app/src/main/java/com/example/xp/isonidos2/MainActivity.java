0c870338ece6cbf11e73cf1d5e157ad9b1d4fc11
xp <gon98saez@gmail.com>
2018-04-23 11:18:56 +0200

    Arreglado problema que al enviar no se enviaba bien el fichero.

--- app/src/main/java/com/example/xp/isonidos2/MainActivity.java
+++ app/src/main/java/com/example/xp/isonidos2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.xp.isonidos2;
 
+import android.content.ContentResolver;
 import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.graphics.Color;
@@ -12,9 +16,19 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.widget.VideoView;
+import android.content.Intent;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Field;
+
+import static android.support.v4.content.FileProvider.getUriForFile;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LinearLayout principal = findViewById(R.id.botones);
-
 
 
         int numeroLinea = 0;
@@ -44,16 +57,77 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    private static final String SHARED_PROVIDER_AUTHORITY = BuildConfig.APPLICATION_ID + ".myfileprovider";
+    private static final String SHARED_FOLDER = "shared";
+
+    public void sonidoCopiar(View view) throws IOException{
+
+        Button b = (Button) findViewById(view.getId());
+        String nombre = b.getText().toString();
+        String extension = ".mp3";
+        String tipo = "audio/mpeg";
+        if (nombre.substring(0,2).contains("v_")) {
+            extension = ".mp4";
+            tipo = "video/mp4";
+        }
+        InputStream ins = getResources().openRawResource(
+                getResources().getIdentifier(nombre,
+                        "raw", getPackageName()));
+
+        final File sharedFolder = new File(getFilesDir(), SHARED_FOLDER);
+        sharedFolder.mkdirs();
+
+        final File sharedFile = File.createTempFile(nombre,extension , sharedFolder);
+        sharedFile.createNewFile();
+
+        copyInputStreamToFile (ins, sharedFile);
+        final Uri uri = FileProvider.getUriForFile(this, SHARED_PROVIDER_AUTHORITY, sharedFile);
+        final ShareCompat.IntentBuilder intentBuilder = ShareCompat.IntentBuilder.from(this)
+                .setType(tipo)
+                .addStream(uri);
+        final Intent chooserIntent = intentBuilder.createChooserIntent();
+        startActivity(chooserIntent);
+    }
+
+    private void copyInputStreamToFile(InputStream in, File file) {
+        OutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            // Ensure that the InputStreams are closed even if there's an exception.
+            try {
+                if ( out != null ) {
+                    out.close();
+                }
+                in.close();
+            }
+            catch ( IOException e ) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     public void sonido(View view) {
-        Log.i("etiqueta: ", findViewById(view.getId()).getTag().toString());
+        //Log.i("etiqueta: ", findViewById(view.getId()).getTag().toString());
         Button b = (Button) findViewById(view.getId());
         String name = b.getText().toString();
-        if(name.contains("v_")){
+        if (name.contains("v_")) {
             VideoView videoview = (VideoView) findViewById(R.id.videoView);
-            Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+view.getTag());
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + view.getTag());
             videoview.setVideoURI(uri);
             videoview.start();
-        }else{
+        } else {
             MediaPlayer m = new MediaPlayer();
             m = MediaPlayer.create(this, (int) findViewById(view.getId()).getTag());
             m.start();
@@ -90,8 +164,8 @@ public class MainActivity extends AppCompatActivity {
         Button b = new Button(this);
         b.setLayoutParams(parametrosBotones);
         b.setText(_listaCanciones[i].getName());
-        b.setTextColor(Color.WHITE);
-        b.setBackgroundColor(Color.BLUE);
+        b.setTextColor(Color.CYAN);
+        b.setBackgroundColor(Color.GREEN);
         b.setAllCaps(false); //todas las letras del botón en minúscula
         int id = this.getResources().getIdentifier(_listaCanciones[i].getName(), "raw", this.getPackageName());
         b.setTag(id);
@@ -101,6 +175,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sonido(view);
+            }
+        });
+
+        b.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                try {
+                    sonidoCopiar(v);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return true;
             }
         });
         return b;
